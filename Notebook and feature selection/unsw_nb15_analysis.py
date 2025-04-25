a8b3a2e4065d25ec8021661829fdd1599f820ff2
Almaz Numan <almaz.20211245@iit.ac.lk>
2025-04-25 17:04:42 +0530

    Update unsw_nb15_analysis.py

--- Notebook and feature selection/unsw_nb15_analysis.py
+++ Notebook and feature selection/unsw_nb15_analysis.py
@@ -13,8 +13,8 @@ torch.manual_seed(42)
 np.random.seed(42)
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Using device: {device}")
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# print(f"Using device: {device}")
 
 
 def preprocessData(train_path, test_path):
